6cb7373e570e0f1b0d4c1f328d763433ac14d4d6
huxy0510 <huxy0510@thundersoft.com>
2016-10-21 16:43:08 +0800

    CoolWeather: create database table,And Each table corresponds Entity class(javaBean).
    type: Study
    src: Open

---
+++ src/com/zkcd/coolweather/db/CoolWeatherOpenHelper.java
@@ -0,0 +1,57 @@
+package com.zkcd.coolweather.db;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+/**
+ *
+ * @author huxiyang
+ *
+ */
+public class CoolWeatherOpenHelper extends SQLiteOpenHelper{
+    /**
+     * Province建表语句
+     */
+    private static final String CREATE_PROVINCE = " create table Province ( "
+            + "id integer primary key autoincrement, "
+            + "province_name text, "
+            + "province_code text)";
+
+    /**
+     * City建表语句
+     */
+    private static final String CREATE_CITY = " create table City ( "
+            + "id integer primary key autoincrement, "
+            + "city_name text, "
+            + "city_code text, "
+            + "province_id integer)";
+
+    /**
+     * County建表语句
+     */
+    private static final String CREATE_COUNTY = " create table County ( "
+            + "id integer primary key autoincrement, "
+            + "county_name text, "
+            + "county_code text, "
+            + "city_id integer)";
+
+    public CoolWeatherOpenHelper(Context context, String name,
+            CursorFactory factory, int version) {
+        super(context, name, factory, version);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_PROVINCE);  //创建Province表
+        db.execSQL(CREATE_CITY);
+        db.execSQL(CREATE_COUNTY);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        // TODO Auto-generated method stub
+    }
+
+}
